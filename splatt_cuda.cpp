446728f0212c30652b3c1b6ee1bce3b837c9e4d7
cshiles <cshiles@qbert.(none)>
2015-03-25 22:45:34 -0400

    screwing up, screewing down

--- splatt_cuda.cpp
+++ splatt_cuda.cpp
@@ -1,74 +1,29 @@
-#include <splatter.h>
-#include <splatt_geom.h>
+#include <vector>
+#include <stdio.h>
 
-using namespace splatter;
 
 double calculate_volumes(double* vertices, int num_vertices,
                          int* tets, int num_tets);
 
+using namespace std;
+
 int main(int argc, char** argv)
 {
-    MPI_Init(&argc, &argv);
+    vector<double> coords;
+    vector<int> tets;
 
-    // the starcd loader will populate these
-    std::vector<starcd::node_coords>	coords; // node coordinates
-    std::vector<int>			tets;	// tet nodes
-    std::vector<starcd::tet_cond_data>	vcond;	// tet volume conditions
-    std::vector<int>			bnds;	// boundary (triangle) nodes
-    std::vector<starcd::bnd_cond_data>	bcond;	// boundary conditions
+    int i;
+    double c[] = {0,0,0,1,0,1};//,0,1,1,2,2,2
+    for(i=0;i<6;i++)
+      coords.push_back(c[i]);
 
-    
-    LLOG(0, "in the beginning....");
-
-    // this guy is in charge of everything -- load it with the
-    // standard mesh entities, default MPI configuration,
-    // and initialize immediately
-    part_mgr mgr(topo::num_std_entities, topo::std_entities);
-
-    // Timers to analyze performance of routines
-    timer total_timer;
-    timer part_timer;
-
-
-    // reroute std(err|out) to files
-    if (mgr.pctx().np() > 1)
-    {
-	mgr.pctx().rebind_stdio();
-    }
-
-    char load_file[50] = "small";
-
-    if (starcd::load_all(load_file, mgr.pctx(), coords, tets, vcond, bnds, bcond) == SPLATT_FAIL)
-    {
-        ERROR("load failed");
-        MPI_Abort(mgr.pctx().comm(), -1);
-    }
-
-    // build node distribution out of local vector sizes
-    mgr.config_node_index(coords.size());
-    mgr.finalize_load();
-
-    LLOG(0, "log/finalize complete");
-    LOG_MEM(-1, "predecomp");
-    LLOG(0, "number of coords: " << coords.size());
-    LLOG(0, "number of tets: " << (tets.size()/4));
-    LLOG(0, "number of bnds: " << (bnds.size()/3));
-
-
-//calculate volume host side
-    double host_vol = 0, tmp;
-    unsigned int i=0; while(i<tets.size()){
-	tmp = geom::tet_volume(coords[tets[i]], coords[tets[i+1]], 
-			       coords[tets[i+2]], coords[tets[i+3]]);
-	printf("%f\n",tmp);
-	host_vol += tmp; 
-	i += 4;
-    }
-    LLOG(0, "host answer:" << host_vol);
+    int t[] = {50,1,2,3};
+    for(i=0;i<4;i++)
+      tets.push_back(t[i]);
 
 //calculate volume device side
     double device_vol =calculate_volumes((double*)&coords[0], coords.size(), (int*)&tets[0], tets.size()/4);
-    LLOG(0, "device answer: " << device_vol);
+    printf("woo %f\n", device_vol);
 }
 
 
--- tet_vols.cu
+++ tet_vols.cu
@@ -44,6 +44,16 @@ double dot(double* a, double* b)
 }
 
 __global__
+void wet_volume(double* vertices, int num_vertices,
+                           int* tets, int num_tets, double* ans)
+{
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (i >= num_tets) return;
+    else ans[i] = 60;
+}
+
+__global__
 void tet_volume(double* vertices, int num_vertices,
                            int* tets, int num_tets, double* ans)
 {
@@ -94,7 +104,7 @@ double calculate_volumes(double* vertices, int num_vertices,
     CUDA_ERR_CHECK(cudaMemcpy(cuda_vertices, vertices, num_vertices*3*sizeof(double), cudaMemcpyHostToDevice));
     CUDA_ERR_CHECK(cudaMemcpy(cuda_tets, tets, num_tets*4*sizeof(int), cudaMemcpyHostToDevice));
 
-    tet_volume<<<blocksPerGrid, threadsPerBlock>>>(cuda_vertices, num_vertices, cuda_tets, num_tets, cuda_sizes);
+    wet_volume<<<blocksPerGrid, threadsPerBlock>>>(cuda_vertices, num_vertices, cuda_tets, num_tets, cuda_sizes);
     
 //    CUDA_ERR_CHECK(cudaGetLastError());
 //Previous line gives me this error: 'Error "invalid device function " at tet_vols.cu:53'
